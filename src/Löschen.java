bc5310b3e079e981cd6be4f9c31ae171927cbc42
PaulK <IA21A.Koester.Paul@bk-hennef.de>
2023-03-16 16:59:45 +0100

    start

--- src/Löschen.java
+++ src/Löschen.java
@@ -14,6 +14,7 @@ public class Löschen {
         if (response == 0) {
             try (Connection conn = DriverManager.getConnection(url, "root", "")) {
                 Statement s = conn.createStatement();
+                //SELECT *, CURRENT_TIMESTAMP FROM spiel WHERE spiel.Heim =1 or spiel.Gast =1 AND spiel.Spiel_ID NOT IN (SELECT b.Spiel_ID FROM bundesliga_archiv.spiel as b);
                 ResultSet r = s.executeQuery("INSERT into bundesliga_archiv.verein SELECT * ,CURRENT_TIMESTAMP FROM `verein` WHERE V_ID = " + Einzelansicht.vid);
                 r = s.executeQuery("DELETE  From " + tab + " WHERE " + model.getColumnName(0) + "=" + Einzelansicht.vid);
 
